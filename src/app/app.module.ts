a050d56c5ebcff3aa608cd5ab76f2096d081c3de
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-08-24 22:23:24 -0300

    Criação do projeto e do módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TictactoeModule } from './tictactoe';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TictactoeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
