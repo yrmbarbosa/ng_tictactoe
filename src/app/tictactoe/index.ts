07ba32d58f6fe2a8fde3ef68ee62ff79adc994fc
Yuri Barbosa <yrmbarbosa@gmail.com>
2020-08-25 22:06:42 -0300

    Criação do componente e serviço do jogo da velha

--- src/app/tictactoe/index.ts
+++ src/app/tictactoe/index.ts
@@ -1,1 +1,3 @@
 export * from './tictactoe.module';
+export * from './tictactoe.component';
+export * from './shared';
--- src/app/tictactoe/tictactoe.module.ts
+++ src/app/tictactoe/tictactoe.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TictactoeComponent } from './tictactoe.component';
+import { TictactoeService } from './shared'
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TictactoeComponent
+  ],
+  exports: [
+    TictactoeComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    TictactoeService
   ]
 })
 export class TictactoeModule { }
